be6288425bf4a0cbef9bc1e55001284f9a7c1eed
dakshj31 <dakshjoshi1355@gmail.com>
2025-03-29 12:41:59 +0530

    3

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Query\IndexHint;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 use App\Models\Task;
-
+use Illuminate\Http\Request;
 
 Route::get('/', function() {
     return redirect()->route('tasks.index');
@@ -12,13 +12,20 @@ Route::get('/', function() {
 
 Route::get('/tasks', function () {
     return view('index', [
-        'tasks' => Task::latest()
+        'tasks' => Task::latest()->get()
     ]);
 })->name('tasks.index');
+
+Route::view('/tasks/create', 'create')
+->name('tasks.create');
 
 Route::get('/tasks/{id}', function ($id) {
     return view('show', ['task'=> Task::findOrFail($id)]);
 })->name('tasks.show');
+
+Route::post('/tasks', function (Request $request) {
+    dd($request-> all());
+})->name('tasks.store');
 
 Route::fallback(function () {
     return 'Still got somewhere!';
