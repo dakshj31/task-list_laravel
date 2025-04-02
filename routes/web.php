075fa52044afca8e39fb80eb8415df51adfbe567
dakshj31 <dakshjoshi1355@gmail.com>
2025-04-02 22:49:17 +0530

    tailwind added

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::get('/', function() {
 
 Route::get('/tasks', function () {
     return view('index', [
-        'tasks' => Task::latest()->get()
+        'tasks' => Task::latest()->paginate(10)
     ]);
 })->name('tasks.index');
 
@@ -29,26 +29,14 @@ Route::get('/tasks/{task}', function (Task $task) {
 })->name('tasks.show');
 
 Route::post('/tasks', function (TaskRequest $request) {
-    // $data = $request->validated();
-    // $task = new Task;
-    // $task->title = $data['title'];
-    // $task->description = $data['description'];
-    // $task->long_description = $data['long_description'];
-    // $task->save();
     $task = Task::create($request->validated());
-
     return redirect()->route('tasks.show', ['task' => $task->id])
     ->with('success', 'Task created successfully');
 })->name('tasks.store');
 
 Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
     $data = $request->validated();
-    // $task->title = $data['title'];
-    // $task->description = $data['description'];
-    // $task->long_description = $data['long_description'];
-    // $task->save();
     $task->update($request->validated());
-
     return redirect()->route('tasks.show', ['task' => $task->id])
     ->with('success', 'Task updated successfully');
 })->name('tasks.update');
@@ -59,6 +47,11 @@ Route::delete('/tasks/{task}', function (Task $task){
     return redirect()->route('tasks.index')
     ->with('success', 'Task deleted successfully');
 })->name('tasks.destroy');
+
+Route::put('tasks/{task}/toggle-complete', function (Task $task) {
+$task->toggleComplete();
+    return redirect()->back()->with('success', 'Task updated successfully!');
+})->name('tasks.toggle-complete');
 
 Route::fallback(function () {
     return 'Still got somewhere!';
