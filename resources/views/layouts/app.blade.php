9af81e2d3d826e8ef72b4442dab3576d385488b0
dakshj31 <dakshjoshi1355@gmail.com>
2025-04-03 23:07:34 +0530

    task page styled

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,17 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Task List</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+    {{-- balde formatter disable--}}
+    <style type="text/tailwindcss">
+    .btn {
+        @apply rounded-md px-2 py-1 text-center font-medium shadow-sm ring-1 ring-slate-700 hover:bg-slate-50 text-slate-700
+    } 
+    .link {
+        @apply font-medium text-gray-700 underline decoration-pink-500
+    }
+    </style>
+    {{-- balde formatter enable--}}
     @yield('styles')
 </head>
 <body class="container mx-auto mt-10 mb-10 max-w-lg">
